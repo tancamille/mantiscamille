61f7033a47b09388bc081d8baa5a6e9e449ad295
Camille Tan <Camille.Tan@flex.com>
2017-10-24 23:02:09 -0700

    [Studio] Add screen "reportissues"

---
+++ client/controllers/app/reportissues.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_reportissues', app_reportissues);
+function app_reportissues($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.reportissues', {
+        views: {
+            app: {
+                controller: 'app_reportissues',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.reportissues');
+                }
+            }
+        }
     });
 });
